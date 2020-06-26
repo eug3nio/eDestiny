9073e4b735359196ff0a4300ace4300981a56243
eug3nio <eugenio.merlin@hotmail.com>
2020-06-25 23:08:05 -0300

    Criação das rotas e da tela de home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
 
 //COMPONENTES
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 //MODULOS PRIME
 import { InputTextModule } from 'primeng/inputtext';
@@ -11,13 +13,22 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { TooltipModule } from 'primeng/tooltip';
 
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+]
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
+
     InputTextModule,
     ButtonModule,
     CardModule,
